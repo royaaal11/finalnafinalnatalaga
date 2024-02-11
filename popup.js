5ef386395ef17be8aa032f8aa096289405b50063
CoderVerse <139535641+royaaal11@users.noreply.github.com>
2024-02-11 18:27:57 +0800

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,9 @@
+function winGame() {
+    // Show the pop-up container
+    document.getElementById('popupContainer').style.display = 'block';
+}
+
+function closePopup() {
+    // Hide the pop-up container
+    document.getElementById('popupContainer').style.display = 'none';
+}
